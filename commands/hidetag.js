f5a343b368fe3ca27ad59e925caa7d1de419a67b
Spencer Official <arcadeskype884@gmail.com>
2026-07-03 16:26:01 +0300

    Update hidetag.js

--- commands/hidetag.js
+++ commands/hidetag.js
@@ -3,7 +3,9 @@ module.exports = {
     name: 'hidetag',
     async execute(sock, msg, args) {
         const from = msg.key.remoteJid;
-        if (!from.endsWith('@g.us')) return;
+        if (!from.endsWith('@g.us')) {
+            return await sock.sendMessage(from, { text: '❌ Group only command.' }, { quoted: msg });
+        }
 
         const groupMetadata = await sock.groupMetadata(from);
         const participants = groupMetadata.participants;
