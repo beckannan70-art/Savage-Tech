140556e853a9057670666ad1845c43a9d1cf1420
Spencer Official <arcadeskype884@gmail.com>
2026-07-03 15:13:30 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports = {
     const msgTimestamp = msg.messageTimestamp * 1000;
     const networkLatency = Date.now() - msgTimestamp;
 
-    const text = `⚡ *Savage Tech Speed* ⚡\n\n*Pong!* ${networkLatency} ms\n\n_⚡ Savage-Tech OS_`;
+    const text = `⚡ *Savage Tech Speed* ⚡\n\n${networkLatency} ms\n\n_⚡ Savage-Tech OS_`;
 
     await sock.sendMessage(msg.key.remoteJid, { text: text }, { quoted: msg });
   }
